a375746bfd30aac5815d228d59d58fdc8c734c47
JuancarlosML94 <juancarlosml94@alumnos.ilerna.com>
2026-09-21 13:22:52 +0200

    Aplicar escape seguro en datos dinámicos

--- fichero_demostracion.php
+++ fichero_demostracion.php
@@ -10,11 +10,13 @@
     <?php
         $fechaActual = date('d/m/Y');
         $horaActual = date('H:i:s');
+
+    // Escapamos los datos antes de mostrarlos en HTML para evitar que contenido no confiable se interprete como código (XSS).
     ?>
 
     <h1>Demostración de PHP</h1>
-    <p>Fecha del servidor: <?php echo $fechaActual; ?></p>
-    <p>Hora del servidor: <?php echo $horaActual; ?></p>
-    
+    <p>Fecha del servidor: <?php echo htmlspecialchars($fechaActual); ?></p>
+    <p>Hora del servidor: <?php echo htmlspecialchars($horaActual); ?></p>
+
 </body>
 </html>
